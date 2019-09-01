33ccc8954c28df8d5352638ef9453c169b374214
Hugo Alejandro Ramos Garcia <hramos0@ucol.mx>
2019-09-01 15:51:34 -0500

    Jejeje

---
+++ Calculadoraa/Calculadoraa/Calculator.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace Calculadoraa
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Boton1 = new System.Windows.Forms.Button();
+            this.Boton2 = new System.Windows.Forms.Button();
+            this.Boton3 = new System.Windows.Forms.Button();
+            this.Boton4 = new System.Windows.Forms.Button();
+            this.Boton5 = new System.Windows.Forms.Button();
+            this.Boton6 = new System.Windows.Forms.Button();
+            this.Boton7 = new System.Windows.Forms.Button();
+            this.Boton8 = new System.Windows.Forms.Button();
+            this.Boton9 = new System.Windows.Forms.Button();
+            this.Boton0 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.Botondividir = new System.Windows.Forms.Button();
+            this.Botonmultiplicar = new System.Windows.Forms.Button();
+            this.Botonigual = new System.Windows.Forms.Button();
+            this.Borrrartodo = new System.Windows.Forms.Button();
+            this.Botonrestar = new System.Windows.Forms.Button();
+            this.Botonsumar = new System.Windows.Forms.Button();
+            this.Botonporcentaje = new System.Windows.Forms.Button();
+            this.tbxScreen = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Boton1
+            // 
+            this.Boton1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton1.Location = new System.Drawing.Point(27, 243);
+            this.Boton1.Name = "Boton1";
+            this.Boton1.Size = new System.Drawing.Size(35, 43);
+            this.Boton1.TabIndex = 0;
+            this.Boton1.Text = "1";
+            this.Boton1.UseVisualStyleBackColor = true;
+            this.Boton1.Click += new System.EventHandler(this.Boton1_Click);
+            // 
+            // Boton2
+            // 
+            this.Boton2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton2.Location = new System.Drawing.Point(80, 243);
+            this.Boton2.Name = "Boton2";
+            this.Boton2.Size = new System.Drawing.Size(35, 43);
+            this.Boton2.TabIndex = 1;
+            this.Boton2.Text = "2";
+            this.Boton2.UseVisualStyleBackColor = true;
+            this.Boton2.Click += new System.EventHandler(this.Boton2_Click);
+            // 
+            // Boton3
+            // 
+            this.Boton3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton3.Location = new System.Drawing.Point(131, 243);
+            this.Boton3.Name = "Boton3";
+            this.Boton3.Size = new System.Drawing.Size(35, 43);
+            this.Boton3.TabIndex = 2;
+            this.Boton3.Text = "3";
+            this.Boton3.UseVisualStyleBackColor = true;
+            this.Boton3.Click += new System.EventHandler(this.Boton3_Click);
+            // 
+            // Boton4
+            // 
+            this.Boton4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton4.Location = new System.Drawing.Point(27, 191);
+            this.Boton4.Name = "Boton4";
+            this.Boton4.Size = new System.Drawing.Size(35, 37);
+            this.Boton4.TabIndex = 3;
+            this.Boton4.Text = "4";
+            this.Boton4.UseVisualStyleBackColor = true;
+            this.Boton4.Click += new System.EventHandler(this.Boton4_Click);
+            // 
+            // Boton5
+            // 
+            this.Boton5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton5.Location = new System.Drawing.Point(80, 191);
+            this.Boton5.Name = "Boton5";
+            this.Boton5.Size = new System.Drawing.Size(35, 37);
+            this.Boton5.TabIndex = 4;
+            this.Boton5.Text = "5";
+            this.Boton5.UseVisualStyleBackColor = true;
+            this.Boton5.Click += new System.EventHandler(this.Boton5_Click);
+            // 
+            // Boton6
+            // 
+            this.Boton6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton6.Location = new System.Drawing.Point(131, 191);
+            this.Boton6.Name = "Boton6";
+            this.Boton6.Size = new System.Drawing.Size(35, 37);
+            this.Boton6.TabIndex = 5;
+            this.Boton6.Text = "6";
+            this.Boton6.UseVisualStyleBackColor = true;
+            this.Boton6.Click += new System.EventHandler(this.Boton6_Click);
+            // 
+            // Boton7
+            // 
+            this.Boton7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton7.Location = new System.Drawing.Point(27, 139);
+            this.Boton7.Name = "Boton7";
+            this.Boton7.Size = new System.Drawing.Size(35, 40);
+            this.Boton7.TabIndex = 6;
+            this.Boton7.Text = "7";
+            this.Boton7.UseVisualStyleBackColor = true;
+            this.Boton7.Click += new System.EventHandler(this.Boton7_Click);
+            // 
+            // Boton8
+            // 
+            this.Boton8.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton8.Location = new System.Drawing.Point(80, 139);
+            this.Boton8.Name = "Boton8";
+            this.Boton8.Size = new System.Drawing.Size(35, 40);
+            this.Boton8.TabIndex = 7;
+            this.Boton8.Text = "8";
+            this.Boton8.UseVisualStyleBackColor = true;
+            this.Boton8.Click += new System.EventHandler(this.Boton8_Click);
+            // 
+            // Boton9
+            // 
+            this.Boton9.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton9.Location = new System.Drawing.Point(131, 138);
+            this.Boton9.Name = "Boton9";
+            this.Boton9.Size = new System.Drawing.Size(35, 41);
+            this.Boton9.TabIndex = 8;
+            this.Boton9.Text = "9";
+            this.Boton9.UseVisualStyleBackColor = true;
+            this.Boton9.Click += new System.EventHandler(this.Boton9_Click);
+            // 
+            // Boton0
+            // 
+            this.Boton0.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Boton0.Location = new System.Drawing.Point(27, 300);
+            this.Boton0.Name = "Boton0";
+            this.Boton0.Size = new System.Drawing.Size(88, 43);
+            this.Boton0.TabIndex = 9;
+            this.Boton0.Text = "0";
+            this.Boton0.UseVisualStyleBackColor = true;
+            this.Boton0.Click += new System.EventHandler(this.Boton0_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(131, 300);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(35, 39);
+            this.button11.TabIndex = 10;
+            this.button11.Text = ".";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Borrar
+            // 
+            this.Borrar.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Borrar.Location = new System.Drawing.Point(182, 137);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(36, 41);
+            this.Borrar.TabIndex = 11;
+            this.Borrar.Text = "C";
+            this.Borrar.UseVisualStyleBackColor = true;
+            this.Borrar.Click += new System.EventHandler(this.Borrar_Click);
+            // 
+            // Botondividir
+            // 
+            this.Botondividir.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botondividir.Location = new System.Drawing.Point(182, 191);
+            this.Botondividir.Name = "Botondividir";
+            this.Botondividir.Size = new System.Drawing.Size(36, 37);
+            this.Botondividir.TabIndex = 12;
+            this.Botondividir.Text = "/";
+            this.Botondividir.UseVisualStyleBackColor = true;
+            this.Botondividir.Click += new System.EventHandler(this.Botondividir_Click);
+            // 
+            // Botonmultiplicar
+            // 
+            this.Botonmultiplicar.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botonmultiplicar.Location = new System.Drawing.Point(182, 243);
+            this.Botonmultiplicar.Name = "Botonmultiplicar";
+            this.Botonmultiplicar.Size = new System.Drawing.Size(36, 41);
+            this.Botonmultiplicar.TabIndex = 13;
+            this.Botonmultiplicar.Text = "*";
+            this.Botonmultiplicar.UseVisualStyleBackColor = true;
+            this.Botonmultiplicar.Click += new System.EventHandler(this.Botonmultiplicar_Click);
+            // 
+            // Botonigual
+            // 
+            this.Botonigual.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botonigual.Location = new System.Drawing.Point(182, 300);
+            this.Botonigual.Name = "Botonigual";
+            this.Botonigual.Size = new System.Drawing.Size(36, 39);
+            this.Botonigual.TabIndex = 14;
+            this.Botonigual.Text = "=";
+            this.Botonigual.UseVisualStyleBackColor = true;
+            this.Botonigual.Click += new System.EventHandler(this.Botonigual_Click);
+            // 
+            // Borrrartodo
+            // 
+            this.Borrrartodo.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Borrrartodo.Location = new System.Drawing.Point(235, 138);
+            this.Borrrartodo.Name = "Borrrartodo";
+            this.Borrrartodo.Size = new System.Drawing.Size(41, 40);
+            this.Borrrartodo.TabIndex = 15;
+            this.Borrrartodo.Text = "CE";
+            this.Borrrartodo.UseVisualStyleBackColor = true;
+            this.Borrrartodo.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // Botonrestar
+            // 
+            this.Botonrestar.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botonrestar.Location = new System.Drawing.Point(235, 191);
+            this.Botonrestar.Name = "Botonrestar";
+            this.Botonrestar.Size = new System.Drawing.Size(41, 40);
+            this.Botonrestar.TabIndex = 16;
+            this.Botonrestar.Text = "-";
+            this.Botonrestar.UseVisualStyleBackColor = true;
+            this.Botonrestar.Click += new System.EventHandler(this.Botonrestar_Click);
+            // 
+            // Botonsumar
+            // 
+            this.Botonsumar.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botonsumar.Location = new System.Drawing.Point(235, 245);
+            this.Botonsumar.Name = "Botonsumar";
+            this.Botonsumar.Size = new System.Drawing.Size(41, 37);
+            this.Botonsumar.TabIndex = 17;
+            this.Botonsumar.Text = "+";
+            this.Botonsumar.UseVisualStyleBackColor = true;
+            this.Botonsumar.Click += new System.EventHandler(this.Botonsumar_Click);
+            // 
+            // Botonporcentaje
+            // 
+            this.Botonporcentaje.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botonporcentaje.Location = new System.Drawing.Point(235, 300);
+            this.Botonporcentaje.Name = "Botonporcentaje";
+            this.Botonporcentaje.Size = new System.Drawing.Size(41, 41);
+            this.Botonporcentaje.TabIndex = 18;
+            this.Botonporcentaje.Text = "%";
+            this.Botonporcentaje.UseVisualStyleBackColor = true;
+            this.Botonporcentaje.Click += new System.EventHandler(this.Botonporcentaje_Click);
+            // 
+            // tbxScreen
+            // 
+            this.tbxScreen.Location = new System.Drawing.Point(27, 20);
+            this.tbxScreen.Multiline = true;
+            this.tbxScreen.Name = "tbxScreen";
+            this.tbxScreen.ReadOnly = true;
+            this.tbxScreen.Size = new System.Drawing.Size(261, 79);
+            this.tbxScreen.TabIndex = 19;
+            this.tbxScreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbxScreen.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 373);
+            this.Controls.Add(this.tbxScreen);
+            this.Controls.Add(this.Botonporcentaje);
+            this.Controls.Add(this.Botonsumar);
+            this.Controls.Add(this.Botonrestar);
+            this.Controls.Add(this.Borrrartodo);
+            this.Controls.Add(this.Botonigual);
+            this.Controls.Add(this.Botonmultiplicar);
+            this.Controls.Add(this.Botondividir);
+            this.Controls.Add(this.Borrar);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.Boton0);
+            this.Controls.Add(this.Boton9);
+            this.Controls.Add(this.Boton8);
+            this.Controls.Add(this.Boton7);
+            this.Controls.Add(this.Boton6);
+            this.Controls.Add(this.Boton5);
+            this.Controls.Add(this.Boton4);
+            this.Controls.Add(this.Boton3);
+            this.Controls.Add(this.Boton2);
+            this.Controls.Add(this.Boton1);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Boton1;
+        private System.Windows.Forms.Button Boton2;
+        private System.Windows.Forms.Button Boton3;
+        private System.Windows.Forms.Button Boton4;
+        private System.Windows.Forms.Button Boton5;
+        private System.Windows.Forms.Button Boton6;
+        private System.Windows.Forms.Button Boton7;
+        private System.Windows.Forms.Button Boton8;
+        private System.Windows.Forms.Button Boton9;
+        private System.Windows.Forms.Button Boton0;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button Borrar;
+        private System.Windows.Forms.Button Botondividir;
+        private System.Windows.Forms.Button Botonmultiplicar;
+        private System.Windows.Forms.Button Botonigual;
+        private System.Windows.Forms.Button Borrrartodo;
+        private System.Windows.Forms.Button Botonrestar;
+        private System.Windows.Forms.Button Botonsumar;
+        private System.Windows.Forms.Button Botonporcentaje;
+        private System.Windows.Forms.TextBox tbxScreen;
+    }
+}
+
---
+++ Calculadoraa/Calculadoraa/Calculator.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadoraa
+{
+    public partial class Calculator : Form
+    {
+        double pri;
+        double seg;
+        string op;
+    
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        Clases.ClsSuma obj = new Clases.ClsSuma();
+        Clases.ClsResta obj2 = new Clases.ClsResta();
+        Clases.ClsMultiplicacion obj3 = new Clases.ClsMultiplicacion();
+        Clases.ClsDivision obj4 = new Clases.ClsDivision();
+        Clases.ClsPorcentaje obj5 = new Clases.ClsPorcentaje();
+
+
+      
+
+        private void Boton6_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "6";
+        }
+
+        private void Boton0_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "0";
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Boton1_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "1";
+        }
+
+        private void Boton2_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "2";
+        }
+
+        private void Boton3_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "3";
+        }
+
+        private void Boton4_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "4";
+        }
+
+        private void Boton5_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "5";
+        }
+
+        private void Boton7_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "7";
+        }
+
+        private void Boton8_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "8";
+        }
+
+        private void Boton9_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Text = tbxScreen.Text + "9";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Botonsumar_Click(object sender, EventArgs e)
+        {
+            op = "+";
+            pri = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void Botonrestar_Click(object sender, EventArgs e)
+        {
+            op = "-";
+            pri = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void Botonmultiplicar_Click(object sender, EventArgs e)
+        {
+            op = "*";
+            pri = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void Botondividir_Click(object sender, EventArgs e)
+        {
+            op = "/";
+            pri = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+
+        private void Botonporcentaje_Click(object sender, EventArgs e)
+        {
+            op = "%";
+            pri = double.Parse(tbxScreen.Text);
+            tbxScreen.Clear();
+        }
+        private void Botonigual_Click(object sender, EventArgs e)
+        {
+            seg = double.Parse(tbxScreen.Text);
+
+            double su;
+            double re;
+            double mu;
+            double di;
+            double po;
+
+            switch (op)
+            {
+                case "+":
+                    su = obj.Sumar((pri), (seg));
+                    tbxScreen.Text = su.ToString();
+                    break;
+                case "-":
+                    re = obj2.Restar((pri), (seg));
+                    tbxScreen.Text = re.ToString();
+                    break;
+                case "*":
+                    mu = obj3.Multiplicar((pri), (seg));
+                    tbxScreen.Text = mu.ToString();
+                    break;
+                case "/":
+                    di = obj4.Dividir((pri), (seg));
+                    tbxScreen.Text = di.ToString();
+                    break;
+                case "%":
+                    po = obj5.Porcentaje((pri), (seg));
+                    tbxScreen.Text = po.ToString();
+                    break;
+            }
+        }
+
+        private void Borrar_Click(object sender, EventArgs e)
+        {
+            if (tbxScreen.Text.Length == 1)
+                tbxScreen.Text = "";
+            else
+                tbxScreen.Text = tbxScreen.Text.Substring(0, tbxScreen.Text.Length - 1);
+        }
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            tbxScreen.Clear();
+        }
+    }
+
+}
